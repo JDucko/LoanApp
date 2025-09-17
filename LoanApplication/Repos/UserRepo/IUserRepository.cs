9e051ffbd078999414ac841383575784f9e25f29
JDucko <joel.duco@gmail.com>
2025-09-17 18:22:56 -0400

    fixed user repo issue

--- LoanApplication/Repos/UserRepo/IUserRepository.cs
+++ LoanApplication/Repos/UserRepo/IUserRepository.cs
@@ -1,9 +1,10 @@
-
 using LoanApplication.Entities;
+using System.Threading.Tasks;
+
 
 namespace LoanApplication.Repos.UserRepo
 {
-    public interface IUserRepository : LoanApplication.Repos.Base.IRepoBase<User>
+    public interface IUserRepository : LoanApplication.Repos.Base.IRepoBase<User, int>
     {
         //Add User-specific methods here if needed
     }
--- LoanApplication/Repos/UserRepo/UserRepository.cs
+++ LoanApplication/Repos/UserRepo/UserRepository.cs
@@ -1,9 +1,13 @@
 
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using LoanApplication.Entities;
+using LoanApplication.Data;
+using LoanApplication.Repos.Base;
 
 namespace LoanApplication.Repos.UserRepo
 {
-    public class UserRepository : LoanApplication.Repos.Base.RepoBase<User>, IUserRepository
+    public class UserRepository : RepoBase<User, int>, IUserRepository
     {
         public UserRepository(Context context) : base(context)
         {
