e5df6d20949e5450112adace1858636fec2c1af5
JDucko <joel.duco@gmail.com>
2025-09-17 16:48:22 -0400

    registered user repository

--- LoanApplication/Repos/RepositoriesModule.cs
+++ LoanApplication/Repos/RepositoriesModule.cs
@@ -7,13 +7,17 @@ public class RepositoriesModule : Module
     protected override void Load(ContainerBuilder builder)
     {
         // register open-generic repository base
-        builder.RegisterGeneric(typeof(LoanApplication.Repos.Base.RepoBase<,>))
-            .As(typeof(LoanApplication.Repos.Base.IRepoBase<,>))
+        builder.RegisterGeneric(typeof(Repos.Base.RepoBase<,>))
+            .As(typeof(Repos.Base.IRepoBase<,>))
             .InstancePerLifetimeScope();
 
         // Concrete repository registrations
-        builder.RegisterType<LoanApplication.Repos.LoanRepository>()
-            .As<LoanApplication.Repos.ILoanRepository>()
+        builder.RegisterType<Repos.LoanRepository>()
+            .As<Repos.ILoanRepository>()
+            .InstancePerLifetimeScope();
+
+        builder.RegisterType<Repos.UserRepo.UserRepository>()
+            .As<Repos.UserRepo.IUserRepository>()
             .InstancePerLifetimeScope();
     }
 }
