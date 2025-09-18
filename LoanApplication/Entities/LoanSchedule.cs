7412dd29dc16870f7a3e9e42b53e986de0ca2176
JDucko <joel.duco@gmail.com>
2025-09-18 10:50:57 -0400

    created new entity

---
+++ LoanApplication/Entities/LoanSchedule.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace LoanApplication.Entities
+{
+    public class LoanSchedule : IEntity<int>
+    {
+        public int Id { get; set; }
+        public int LoanId { get; set; }
+        public int Month { get; set; }
+        public decimal MonthlyPayment { get; set; }
+        public decimal RemainingBalance { get; set; }
+
+        public required Loan Loan { get; set; }
+    }
+}
