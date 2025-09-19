bf433939c8d1033ef9c8104acc23ae35f8686c9d
JDucko <joel.duco@gmail.com>
2025-09-19 16:31:11 -0400

    clean up

--- LoanApplication/Repos/LoanScheduleRepo/ILoanScheduleRepository.cs
+++ LoanApplication/Repos/LoanScheduleRepo/ILoanScheduleRepository.cs
@@ -1,7 +1,8 @@
+using LoanApplication.Entities;
 
 namespace LoanApplication.Repos
 {
-    public interface ILoanScheduleRepository : LoanApplication.Repos.Base.IRepoBase<Entities.LoanSchedule, int>
+    public interface ILoanScheduleRepository : LoanApplication.Repos.Base.IRepoBase<LoanSchedule, int>
     {
         // Add LoanSchedule-specific methods here if needed
     }
